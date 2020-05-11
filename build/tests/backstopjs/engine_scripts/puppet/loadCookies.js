bc188828b182017c869b7fab27a2544c40560fb2
Pete Inge <pete.inge@gmail.com>
2020-05-11 10:43:47 -0400

    Helps to actually add the engine scripts…

---
+++ build/tests/backstopjs/engine_scripts/puppet/loadCookies.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+module.exports = async (page, scenario, vp, isReference, Engine) => {
+  console.log("Baking the cookies!!");
+  console.log(isReference);
+  let cookieFile = "";
+
+  if (isReference) {
+    cookieFile = './backstop_data/cookies/cookies_reference.json';
+  } else {
+    cookieFile = './backstop_data/cookies/cookies_test.json';
+  }
+
+  console.log(cookieFile);
+
+  // Check if file exists.
+  if (!fs.existsSync(cookieFile)) {
+    // If not load cookies.
+    console.log("cookies do not exists.");
+  }
+  else {
+    console.log("cookie jar is full.");
+  }
+
+  let cookieData = fs.readFileSync(cookieFile);
+  let cookies = JSON.parse(cookieData);
+
+  // SET COOKIES
+  const setCookies = async () => {
+    return Promise.all(
+      cookies.map(async (cookie) => {
+        console.log(cookie);
+        await page.setCookie(cookie);
+      })
+    );
+  };
+  await setCookies();
+  console.log('Cookie state restored with:', JSON.stringify(cookies, null, 2));
+
+};
---
+++ build/tests/backstopjs/engine_scripts/puppet/onBefore.js
@@ -0,0 +1,6 @@
+
+module.exports = async (page, scenario, vp, isReference, Engine) => {
+
+  if (scenario.auth)
+    await require('./loadCookies')(page, scenario, vp, isReference, Engine);
+};

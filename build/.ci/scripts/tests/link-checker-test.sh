51c0621968bd88f570a2d2aa9a467d9e637df870
Pete Inge <pete.inge@gmail.com>
2021-03-03 16:54:18 -0500

    Add in Broken link Checker

--- build/.ci/scripts/tests/link-checker-test.sh
+++ build/.ci/scripts/tests/link-checker-test.sh
@@ -7,13 +7,17 @@
 ##
 echo -e "Setting up variables...\n"
 
+ARTIFACTS_URL_BASE="$CIRCLE_ARTIFACTS_URL/blc"
+
 BASE_EXPORT_DIR="/tmp/workspace/blc"
 
-BLC_REPORT_URL_BASE="$CIRCLE_ARTIFACTS_URL/blc"
 BLC_REPORT_NAME="blc-result.txt"
+BLC_REPORT="$BASE_EXPORT_DIR/$BLC_REPORT_NAME"
 
-BLC_BASH_ENV="/tmp/workspace/bash/blc_bash_env.txt"
+# BLC_BASH_ENV="/tmp/workspace/bash/blc_bash_env.txt"
 
+echo -e "Make bash directory\n"
+mkdir -p /tmp/workspace/bash
 
 
 
@@ -25,19 +29,19 @@ mkdir -p ${BASE_EXPORT_DIR}
 
 
 
-
-
 ##
 # Run tests
 ##
 echo -e "Running test...\n"
-blc ${LIVE_SITE_URL} --filter-level 0 -fro > "$BASE_EXPORT_DIR/$BLC_REPORT_NAME"
-
+# blc ${LIVE_SITE_URL} --filter-level 0 -fro > "$BASE_EXPORT_DIR/$BLC_REPORT_NAME"
+node ./.ci/scripts/tests/blc.js ${LIVE_SITE_URL} ${BASE_EXPORT_DIR} ${BLC_REPORT_NAME}
 echo -e "Finished running test...\n"
 
+REPORT_MSG=$(cat $BLC_REPORT | jq -r '.["msg"]')
+ERROR_COUNT=$(cat $BLC_REPORT | jq '.["errorCount"] | tonumber')
 
-
-
+echo -e "$REPORT_MSG"
+echo -e "$ERROR_COUNT"
 
 
 
@@ -46,17 +50,21 @@ echo -e "Finished running test...\n"
 ##
 echo -e "Creating PR message...\n"
 BLC_PR_MESSAGE="## $ICON_REPORT Broken Link Checker Report:\n\n"
-BLC_PR_MESSAGE="$BLC_PR_MESSAGE[View Test]("$BLC_REPORT_URL_BASE/$BLC_REPORT_NAME")\n\n"
+BLC_PR_MESSAGE+="Message: $REPORT_MSG\n"
+BLC_PR_MESSAGE+="Errors: $ERROR_COUNT\n\n"
+BLC_PR_MESSAGE="$BLC_PR_MESSAGE[View Test]("$ARTIFACTS_URL_BASE/$BLC_REPORT_NAME")\n\n"
 BLC_PR_MESSAGE="$BLC_PR_MESSAGE[CircleCI Job $CI_BUILD_NUMBER $ICON_ARROW]($CI_BUILD_URL)"
 
 echo -e "$BLC_PR_MESSAGE"
 
 # Set gloabl vars for this job.
+BLC_BASH_ENV=${BLC_BASH_ENV:-$HOME/.blcbashrc}
 (
   echo "export BLC_PR_MESSAGE='$BLC_PR_MESSAGE'"
 ) >> $BLC_BASH_ENV
 
-
+cp $BLC_BASH_ENV  /tmp/workspace/bash/blc_bash_env.txt
+cp $BLC_BASH_ENV  /tmp/workspace/blc/blc_bash_env.txt
 
 
 
--- build/.ci/scripts/tests/scheduled-results.sh
+++ build/.ci/scripts/tests/scheduled-results.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-source "/tmp/workspace/bash/wpt_bash_env.txt"
-source "/tmp/workspace/bash/blc_bash_env.txt"
+# source "/tmp/workspace/bash/wpt_bash_env.txt"
+# source "/tmp/workspace/bash/blc_bash_env.txt"
 
 # Setup Issue Params
-TITLE="Scheduled Automated Test Results: $(date '+%Y-%m-%d')"
+TITLE="Scheduled Automated Test Results $CIRCLE_BUILD_NUM: $(date '+%Y-%m-%d')"
 MESSAGE="# Scheduled Tasks report\n\n"
-MESSAGE="$MESSAGE$WPT_PR_MESSAGE\n\n---\n\n$BLC_PR_MESSAGE"
+# MESSAGE="$MESSAGE$WPT_PR_MESSAGE\n\n---\n\n$BLC_PR_MESSAGE"
+
+# Web Page Test Messages.
+WPT_BASH_ENV=${WPT_BASH_ENV:-$HOME/.blcbashrc}
+cp /tmp/workspace/bash/wpt_bash_env.txt $WPT_BASH_ENV
+source $WPT_BASH_ENV
+MESSAGE="$MESSAGE\n\n---\n\n$WPT_PR_MESSAGE"
+
+
+# Broken Link checker Messages.
+BLC_BASH_ENV=${BLC_BASH_ENV:-$HOME/.blcbashrc}
+cp /tmp/workspace/bash/blc_bash_env.txt $BLC_BASH_ENV
+source $BLC_BASH_ENV
+MESSAGE="$MESSAGE\n\n---\n\n$BLC_PR_MESSAGE"
 
 # Post back to GitHub, by creating a new Issue.
 echo -e "\nPosting results back to to repo issue."
-curl -s -i -u "$CI_PROJECT_USERNAME:$GITHUB_TOKEN" -d "{\"title\": \"$TITLE\", \"body\": \"$MESSAGE\"}" $GITHUB_API_URL/issues > /dev/null
+curl -s -i -u "$CI_PROJECT_USERNAME:$GITHUB_TOKEN" -d "{\"title\": \"$TITLE\", \"body\": \"$MESSAGE\"}" $GITHUB_API_URL/issues

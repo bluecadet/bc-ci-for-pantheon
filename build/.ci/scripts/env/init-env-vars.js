10212d8767d416296cee079bfcea68b21e7cbee0
Pete Inge <pinge@bluecadet.com>
2022-09-29 11:00:44 -0400

    Add in testing for required varaibles in .projectconfig.json

--- build/.ci/scripts/env/init-env-vars.js
+++ build/.ci/scripts/env/init-env-vars.js
@@ -2,5 +2,32 @@
 
 var fs = require('fs');
 const CONFIG = require('../../../.projectconfig.js');
+
+// Check for required variables.
+
+let errs = [];
+if (!CONFIG.TZ) {
+  errs.push("The 'TZ' variable needs to be set in .projectconfig.js");
+}
+if (!CONFIG.TEMP_DIR) {
+  errs.push("The 'TEMP_DIR' variable needs to be set in .projectconfig.js");
+}
+if (!CONFIG.CMS_PLATFORM) {
+  errs.push("The 'CMS_PLATFORM' variable needs to be set in .projectconfig.js");
+}
+if (!CONFIG.DEFAULT_SITE) {
+  errs.push("The 'DEFAULT_SITE' variable needs to be set in .projectconfig.js");
+}
+if (!CONFIG.testingPaths) {
+  errs.push("The 'testingPaths' variable needs to be set in .projectconfig.js");
+}
+
+if (errs.length > 0) {
+  console.log(errs.join("\n"));
+  process.exit(1);
+}
+
+
+
 // Save Config as settings.
 fs.writeFileSync('.projectconfig.json', JSON.stringify(CONFIG, null, 2));
